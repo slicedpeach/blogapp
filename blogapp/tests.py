74cbbb0a619639e50491fd7fd6ba393dc3905fa0
Sam <samansalman@ymail.com>
2021-09-20 12:38:23 +0430

    forms and user accounts managed

--- blogapp/tests.py
+++ blogapp/tests.py
@@ -1,3 +1,5 @@
+
+from django.http import response
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -43,3 +45,24 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'content')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'new title',
+            'body': 'new text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'new title')
+        self.assertEqual(Post.objects.last().body, 'new text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text'
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,8 +1,12 @@
 from os import name
 from django.urls import path
-from .views import BlogDetailView, BlogListView
+from .views import BlogDetailView, BlogListView,BlogCreateView,BlogupdateView,BlogDeleteView
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name = 'home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(),name = 'post_detail')
-    ]
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/' , BlogupdateView.as_view(),name = 'post_edit'),
+    path('post/<int:pk>/delete/',BlogDeleteView.as_view(),name = 'post_delete')
+    
+]
